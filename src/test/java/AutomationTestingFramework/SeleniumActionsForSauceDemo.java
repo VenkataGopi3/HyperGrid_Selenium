396c43b1342f7ce4957b0d03b87aef60e2be174d
Venkata Gopi <bablu19735@gmail.com>
2025-06-10 11:04:20 +0530

    Automation Framework-2

---
+++ src/test/java/AutomationTestingFramework/SeleniumActionsForSauceDemo.java
@@ -0,0 +1,30 @@
+package AutomationTestingFramework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class SeleniumActionsForSauceDemo {
+
+    private WebDriver driver;
+
+    public SeleniumActionsForSauceDemo(WebDriver driver){
+        this.driver = driver;
+
+    }
+    public void clickOnWebElement(WebElement element){
+        element.click();
+    }
+
+    public void clickOnWebElement(By by){
+        driver.findElement(by).click();
+    }
+
+    public void typeValue(WebElement element, String data){
+        element.sendKeys(data);
+    }
+
+    public void typeValue(By by, String data){
+        driver.findElement(by).sendKeys(data);
+    }
+}
